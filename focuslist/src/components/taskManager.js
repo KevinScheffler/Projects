18c1163755af211c5bc2fc158efcd56de3d0b9e4
kevinscheffler <schefk2122@gmail.com>
2024-06-01 17:37:40 -0400

    finished multiple features

---
+++ focuslist/src/components/taskManager.js
@@ -0,0 +1,53 @@
+import { format, isToday, isYesterday, isTomorrow, parseISO } from "date-fns";
+
+const TaskManager = (() => {
+  let myTasks = [];
+  let taskIdCounter = 0;
+
+  function Task(taskName, date, priority, project, notes, id) {
+    this.taskName = taskName;
+    this.date = parseISO(date);
+    this.priority = priority;
+    this.project = project;
+    this.notes = notes;
+    this.id = id;
+  }
+
+  Task.prototype.formatDate = function () {
+    if (isToday(this.date)) {
+      return "Today";
+    }
+
+    if (isYesterday(this.date)) {
+      return "Yesterday";
+    }
+
+    if(isTomorrow(this.date)) {
+      return "Tomorrow";
+    }
+
+    return format(this.date, "EEEE, MMMM do, yyyy");
+  };
+
+  function addTask(task) {
+    myTasks.push(task);
+    taskIdCounter++;
+  }
+
+  function getTasks() {
+    return myTasks;
+  }
+
+  function getTaskIdCounter() {
+    return taskIdCounter;
+  }
+
+  return {
+    Task,
+    addTask,
+    getTasks,
+    getTaskIdCounter,
+  };
+})();
+
+export default TaskManager;
--- focuslist/src/index.js
+++ focuslist/src/index.js
@@ -1,30 +1,11 @@
 import './style.css';
-
-const sbFormContainer = document.getElementById('sidebar-form-container');
-
-const sbAddTask = document.getElementById('add-task-sidebar');
-
-const sbCloseForm = document.getElementById('sb-close');
-
-sbAddTask.onclick = function() {
-  if (sbFormContainer.classList.contains('sb-form-hidden')) {
-    sbFormContainer.classList.remove('sb-form-hidden');
-  } else {
-    sbFormContainer.classList.add('sb-form-hidden');
-  }
-}
-
-sbCloseForm.onclick = function() {
-  sbFormContainer.classList.add('sb-form-hidden');
-}
+import TaskManager from './components/taskManager';
+import allTasks from './components/allTasks';
+import { format } from "date-fns";
 
 const mainFormContainer = document.getElementById('main-form-container');
 
-const mainAddTaskBtn = document.getElementById('main-add-task');
-
-const mainFormCloseBtn = document.getElementById('main-close');
-
-mainAddTaskBtn.onclick = function() {
+function toggleMainForm() {
   if (mainFormContainer.classList.contains('main-form-hidden')) {
     mainFormContainer.classList.remove('main-form-hidden');
   } else {
@@ -32,6 +13,119 @@ mainAddTaskBtn.onclick = function() {
   }
 }
 
-mainFormCloseBtn.onclick = function() {
-  mainFormContainer.classList.add('main-form-hidden');
+document.getElementById('main-add-task').addEventListener('click', () => {
+ toggleMainForm();
+ formatDisplayDate();
+});
+
+document.getElementById('main-close').addEventListener('click', () => {
+  toggleMainForm();
+});
+
+function formatDisplayDate() {
+  const dateInput = document.getElementById('main-date');
+  
+  // Get the current date
+  const today = new Date();
+
+  dateInput.valueAsDate = today;
+
+  // Format the date to YYYY-MM-DD
+  const minDate = format(today, 'yyyy-MM-dd');
+
+  // Set the minimum date attribute for the date input
+  dateInput.setAttribute('min', minDate);
 }
+
+document.getElementById('main-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  const taskName = document.getElementById('main-task-name').value;
+  const date = document.getElementById('main-date').value;
+  const priority = document.getElementById('main-priority').value;
+  const project = document.getElementById('main-folder-group').value;
+  const notes = document.getElementById('main-notes').value;
+  const id = TaskManager.getTaskIdCounter();
+
+  const task = new TaskManager.Task(taskName, date, priority, project, notes, id);
+  TaskManager.addTask(task);
+  const tasks = TaskManager.getTasks();
+  console.log(tasks);
+  allTasks(tasks);
+
+  const allTasksMain = document.getElementById('main-all-tasks');
+  allTasksMain.textContent = '(' + TaskManager.getTasks().length + ')';
+
+  const sbAllTaskTotal = document.getElementById('sb-total-tasks');
+  sbAllTaskTotal.textContent = TaskManager.getTasks().length;
+
+  document.getElementById('main-form').reset();
+  toggleMainForm();
+});
+
+
+
+
+
+
+const sbFormContainer = document.getElementById('sidebar-form-container');
+
+function toggleSidebarForm() {
+  if (sbFormContainer.classList.contains('sb-form-hidden')) {
+    sbFormContainer.classList.remove('sb-form-hidden');
+  } else {
+    sbFormContainer.classList.add('sb-form-hidden');
+  }
+}
+
+function formatSidebarDate() {
+  const dateInput = document.getElementById('sb-date');
+    
+    // Get the current date
+    const today = new Date();
+  
+    dateInput.valueAsDate = today;
+  
+    // Format the date to YYYY-MM-DD
+    const minDate = format(today, 'yyyy-MM-dd');
+  
+    // Set the minimum date attribute for the date input
+    dateInput.setAttribute('min', minDate);
+}
+
+document.getElementById('add-task-sidebar').addEventListener('click', () => {
+  toggleSidebarForm();
+  formatDisplayDate();
+  formatSidebarDate();
+});
+
+document.getElementById('sb-close').addEventListener('click', () => {
+  sbFormContainer.classList.add('sb-form-hidden');
+});
+
+
+document.getElementById('sb-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const taskName = document.getElementById('sb-task-name').value;
+  const date = document.getElementById('sb-date').value;
+  const priority = document.getElementById('sb-priority').value;
+  const project = document.getElementById('sb-folder-group').value;
+  const notes = document.getElementById('sb-notes').value;
+  const id = TaskManager.getTaskIdCounter();
+
+  const task = new TaskManager.Task(taskName, date, priority, project, notes, id);
+  TaskManager.addTask(task);
+  const tasks = TaskManager.getTasks();
+  console.log(tasks);
+  allTasks(tasks);
+
+  const allTasksMain = document.getElementById('main-all-tasks');
+  allTasksMain.textContent = '(' + TaskManager.getTasks().length + ')';
+
+  const sbAllTaskTotal = document.getElementById('sb-total-tasks');
+  sbAllTaskTotal.textContent = TaskManager.getTasks().length;
+
+  document.getElementById('sb-form').reset();
+  toggleSidebarForm();
+});
